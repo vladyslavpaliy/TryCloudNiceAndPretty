82dd3233665e27393d50e2d1157af388530b1dd2
hannapiestova <hannapiestova95@gmail.com>
2021-02-09 22:15:28 -0600

    started locating dashboard page web elements and working on navigate to method

--- src/test/java/com/trycloud/pages/DashboardPage.java
+++ src/test/java/com/trycloud/pages/DashboardPage.java
@@ -1,7 +1,44 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class DashboardPage {
 //
+
+    @FindBy(className = "user-status-menu-item__toggle user-status-menu-item__toggle--inline")
+    private WebElement status;
+
+    @FindBy(className = "icon action-item__menutoggle icon-fair-day action-item__menutoggle--with-title")
+    private WebElement weather;
+
+    @FindBy(xpath = "//a[@class='edit-panels icon-rename' and text()='Customize']")
+    private WebElement customize;
+
+    @FindBy(css = "svg[class='material-design-icon__svg']")
+    private WebElement search;
+
+
+
+    public DashboardPage(){
+        PageFactory.initElements(Driver.get(), this);
+
+    }
+
+    //TryCloud Navigation
+    public void navigateTo(String tab){
+        String tabXpath= "//li[@data-id='"+tab+"']";
+        WebElement tabElement = Driver.get().findElement(By.xpath(tabXpath));
+        WebDriverWait wait = new WebDriverWait(Driver.get(), 10);
+        wait.until(ExpectedConditions.elementToBeClickable(tabElement)).click();
+
+
+    }
 
 
 }
