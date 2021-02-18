64b4a1c01d3b599cf325d67c12768a8337fc206b
hannapiestova <hannapiestova95@gmail.com>
2021-02-18 15:04:24 -0600

    added utilities to BrowserUtils, waitForPageToLoad and click with JS

--- src/test/java/com/trycloud/utilities/BrowserUtils.java
+++ src/test/java/com/trycloud/utilities/BrowserUtils.java
@@ -1,8 +1,12 @@
 package com.trycloud.utilities;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,4 +39,24 @@ public class BrowserUtils {
 
     }
 
+    /**
+     * this method just waits for page to be loaded.
+     */
+    public static void waitForPageToLoad(long timeOutInSeconds){
+        ExpectedCondition<Boolean> expectation = driver -> ((JavascriptExecutor)driver).executeScript("return document.readyState" ).equals("complete");
+        try{
+            WebDriverWait wait = new WebDriverWait(Driver.get(), timeOutInSeconds);
+            wait.until(expectation);
+        }catch(Throwable error){
+            error.printStackTrace();
+        }
+    }
+
+    /**
+     *simple click method with JS . scrolls and then clicks
+     */
+    public static void clickWithJS(WebElement element){
+        ( (JavascriptExecutor)Driver.get()).executeScript("arguments[0].scrollIntoView(true)", element);
+        ((JavascriptExecutor)Driver.get()).executeScript("arguments[0].click()",element);
+    }
 }
